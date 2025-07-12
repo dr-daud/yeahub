a9b0009e23355c0f744f5a94fdd139db27e5371a
Daud <daud_edilbiev@mail.ru>
2025-07-12 14:36:09 +0300

    header is ready, fonts are connected

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,14 +1,16 @@
+import yeahub_logo from "../../../shared/assets/yeahub_logo.svg";
+
 import styles from "./styles.module.css";
 import "../../../shared/ui/index.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className="container">
+      <div className={`${"container"} ${styles.header__flex}`}>
         <nav className={styles.header__nav}>
           <ul className={styles.header__links}>
             <li className={styles["header__link-item"]}>
-              {/* <img src="" alt="" /> */}
+              <img src={yeahub_logo} alt="" />
             </li>
             <li className={styles["header__link-item"]}>База вопросов</li>
             <li className={styles["header__link-item"]}>Тренажер</li>
@@ -18,7 +20,9 @@ const Header = () => {
           <a href="" className={styles["header__access-link"]}>
             вход
           </a>
-          <button className={styles["header__access-button"]}></button>
+          <button className={`${"button"} ${styles["header__access-button"]}`}>
+            Регистрация
+          </button>
         </div>
       </div>
     </header>
