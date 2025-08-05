9c69cb6f73a3a8ae850642b70f4efba17b90b13a
Daud <daud_edilbiev@mail.ru>
2025-08-05 11:45:35 +0300

    adapt top questions for 1260px

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -2,11 +2,12 @@ import "./button.css";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: string;
+  className?: string;
 }
 
-const Button = ({ children, ...props }: Props) => {
+const Button = ({ children, className, ...props }: Props) => {
   return (
-    <button {...props} className="button">
+    <button {...props} className={`button ${className}`}>
       {children}
     </button>
   );
--- src/widgets/top-questions/ui/TopQuestions.tsx
+++ src/widgets/top-questions/ui/TopQuestions.tsx
@@ -1,3 +1,4 @@
+import Button from "../../../shared/ui/button/Button";
 import figmaIcons from "../assets/figma-icons.png";
 import progress from "../assets/progress.png";
 import user from "../assets/user.png";
@@ -20,15 +21,12 @@ const TopQuestions = () => {
             <p className={`${"body3"} ${styles["top-questions__text"]}`}>
               Развивайте и улучшайте свои навыки на одной платформе: изучайте
               новое, готовьтесь к собеседованиям, погружайтесь в нюансы или
-              обновляйте знания — YeaHub поможет справиться с любой <br />
-              задачей
+              обновляйте знания — YeaHub поможет справиться с любой задачей
             </p>
 
-            <button
-              className={`${"button"} ${styles["top-questions__button"]}`}
-            >
+            <Button className={`${styles["top-questions__button"]}`}>
               Присоединиться
-            </button>
+            </Button>
           </div>
           <div className={styles["top-questions__right"]}>
             <img
