e4e1c764429816c467a0881159bd34e3ce2d5fae
Daud <daud_edilbiev@mail.ru>
2025-08-05 10:34:34 +0300

    adapt track and footer section for 1260px

--- src/widgets/track/ui/Track.tsx
+++ src/widgets/track/ui/Track.tsx
@@ -6,35 +6,32 @@ import person from "../assets/icons/PersonArmsSpread.svg";
 const Track = () => {
   return (
     <section className={styles.track}>
-      <div className={`${"container"} ${styles.track__flex}`}>
-        <div className={styles.track__left}>
-          <h2>
-            отслеживайте свой <br />
-            прогресс
-          </h2>
+      <div className={`${"container"} ${styles.track__grid}`}>
+        <div className={styles.track__title}>
+          <h2>отслеживайте свой прогресс</h2>
           <p className={`${styles.track__subtitle} ${"body3"}`}>
-            YeaHub предоставляет продвинутые инструменты <br />
-            для мониторинга вашего обучения.
+            YeaHub предоставляет продвинутые инструменты для мониторинга вашего
+            обучения.
           </p>
-          <div className={styles["track__inner-flex"]}>
-            <div className={styles["track__inner-wrap"]}>
-              <div className={`${styles["track__icon-wrap"]} ${"icon-wrap"}`}>
-                <img src={person} alt="person" />
-              </div>
-              <p className={`${styles.track__text} ${"body3"}`}>
-                В вашем личном профиле доступна детализированная статистика,
-                включая количество пройденных вопросов и тем.
-              </p>
+        </div>
+        <div className={styles["track__inner-flex"]}>
+          <div className={styles["track__inner-wrap"]}>
+            <div className={`${styles["track__icon-wrap"]} ${"icon-wrap"}`}>
+              <img src={person} alt="person" />
             </div>
-            <div className={styles["track__inner-wrap"]}>
-              <div className={`${styles["track__icon-wrap"]} ${"icon-wrap"}`}>
-                <img src={graph} alt="graph" />
-              </div>
-              <p className={`${styles.track__text} ${"body3"}`}>
-                Определите свой текущий уровень и выявите пробелы в знаниях для
-                более целенаправленного обучения или подготовки к поиску работы.
-              </p>
+            <p className={`${styles.track__text} ${"body3"}`}>
+              В вашем личном профиле доступна детализированная статистика,
+              включая количество пройденных вопросов и тем.
+            </p>
+          </div>
+          <div className={styles["track__inner-wrap"]}>
+            <div className={`${styles["track__icon-wrap"]} ${"icon-wrap"}`}>
+              <img src={graph} alt="graph" />
             </div>
+            <p className={`${styles.track__text} ${"body3"}`}>
+              Определите свой текущий уровень и выявите пробелы в знаниях для
+              более целенаправленного обучения или подготовки к поиску работы.
+            </p>
           </div>
         </div>
         <div className={styles.track__right}>
